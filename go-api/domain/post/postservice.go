8bcb189e9e81ad77a764458ae3b3da5c51b6e855
日下陸矢 <rikuya.98@gmail.com>
2025-05-02 15:58:24 +0900

    [add]postのドメイン層の作成

--- go-api/domain/post/postservice.go
+++ go-api/domain/post/postservice.go
@@ -1,1 +1,29 @@
 package domain
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type PostPersistence interface {
+	List(ctx context.Context, workspaceID uuid.UUID) ([]Post, error)
+}
+
+type PostService interface {
+	List(ctx context.Context, workspaceID uuid.UUID) ([]Post, error)
+}
+
+type postService struct {
+	repo PostPersistence
+}
+
+func NewPostService(repo PostPersistence) PostService {
+	return &postService{
+		repo: repo,
+	}
+}
+
+func (s *postService) List(ctx context.Context, workspaceID uuid.UUID) ([]Post, error) {
+	return s.repo.List(ctx, workspaceID)
+}
