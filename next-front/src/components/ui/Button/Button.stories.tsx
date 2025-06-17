c787f393f5218e0bb6fb5d5f11389b433f4411dc
zonoryo17 <p.naka.0p0@gmail.com>
2025-06-17 17:13:00 +0900

    refactor: ButtonコンポーネントのStoryの修正およびドキュメントの追加

--- next-front/src/components/ui/Button/Button.stories.tsx
+++ next-front/src/components/ui/Button/Button.stories.tsx
@@ -1,33 +1,81 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './Button';
-import { action } from '@storybook/addon-actions';
+import Description from './Button.mdx';
 
-const meta: Meta<typeof Button> = {
+const meta = {
   component: Button,
-  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: Description,
+      },
+    },
+  },
   args: {
-    onClick: action('clicked'),
+    children: 'Button',
+    variant: 'primary',
+    size: 'md',
+    type: 'button',
     disabled: false,
+    asChild: false,
+    onClick: () => {},
   },
   argTypes: {
+    onClick: {
+      description: 'ボタンのクリックイベント',
+      action: 'clicked',
+      table: {
+        type: { summary: '() => void' },
+      },
+    },
+    disabled: {
+      description: 'ボタンの無効化状態',
+      control: 'boolean',
+      defaultValue: false,
+      table: {
+        type: { summary: 'boolean' },
+      },
+    },
     type: {
+      description: 'ボタンのタイプ',
       control: 'select',
       options: ['button', 'submit', undefined],
+      defaultValue: 'button',
+      table: {
+        type: { summary: "'button' | 'submit' | undefined" },
+      },
     },
     variant: {
+      description: 'ボタンのバリエーション',
       control: 'select',
       options: ['primary', 'secondary', 'text', 'cancel', 'danger'],
+      defaultValue: 'primary',
+      table: {
+        type: {
+          summary: "'primary' | 'secondary' | 'text' | 'cancel' | 'danger'",
+        },
+      },
     },
     size: {
+      description: 'ボタンのサイズ',
       control: 'select',
       options: ['sm', 'md', 'lg'],
+      defaultValue: 'md',
+      table: {
+        type: { summary: "'sm' | 'md' | 'lg'" },
+      },
     },
     children: {
+      description: 'ボタンのラベル',
       control: 'text',
+      defaultValue: 'Button',
+      table: {
+        type: { summary: 'React.ReactNode' },
+      },
     },
   },
-};
+} satisfies Meta<typeof Button>;
 
 export default meta;
 
@@ -56,5 +104,23 @@ export const Cancel: Story = {
 export const Danger: Story = {
   args: {
     variant: 'danger',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'md',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
   },
 };
