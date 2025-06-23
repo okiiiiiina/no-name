799a409e14f5fd8331fb1e56b87eeda6a5b0e6c7
zonoryo17 <p.naka.0p0@gmail.com>
2025-06-23 11:19:56 +0900

    feat: Checkboxコンポーネントの作成

---
+++ next-front/src/components/ui/Checkbox/index.tsx
@@ -0,0 +1,47 @@
+import styles from './index.module.css';
+
+type Props = {
+  /** チェックボックスのラベル */
+  label: string;
+  /** チェック状態かどうか */
+  checked: boolean;
+  /** チェック状態の変更イベントハンドラ */
+  onChange: (checked: boolean) => void;
+  /** disabled状態の管理 */
+  disabled?: boolean;
+  /** エラー状態の管理 */
+  hasError?: boolean;
+};
+
+export const Checkbox: React.FC<Props> = ({
+  label,
+  checked,
+  onChange,
+  disabled = false,
+  hasError = false,
+}) => {
+  const checkboxStyle = `${styles.checkbox} ${
+    disabled ? styles.disabled : ''
+  } ${hasError ? styles.error : ''}`;
+
+  const containerStyle = `${styles.container} ${
+    disabled ? styles.containerDisabled : ''
+  } ${hasError ? styles.containerError : ''}`;
+
+  const handleClick = () => {
+    if (disabled) return;
+    onChange(!checked);
+  };
+
+  return (
+    <div onClick={handleClick} className={containerStyle}>
+      <input
+        type="checkbox"
+        className={checkboxStyle}
+        checked={checked}
+        disabled={disabled}
+      />
+      <label className={styles.label}>{label}</label>
+    </div>
+  );
+};
